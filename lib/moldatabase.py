61c8040523b3a4b2724f496f545b291ef21c5ca3
James Graham <J.A.Graham@soton.ac.uk>
2016-01-20 17:15:46 +0000

    Refactored MolDatabase

--- lib/moldatabase.py
+++ lib/moldatabase.py
@@ -17,10 +17,6 @@ class MolDatabase:
         fp = FileParser(os.path.join("data", "mol.dat"))
         self.molecules = {}
         Molecule = namedtuple("Molecule", ["atoms", "lengths", "angles", "dihedrals", "impropers"])
-        Length = namedtuple("Length", ["type", "atom1", "atom2"])
-        Angle = namedtuple("Length", ["type", "atom1", "atom2", "atom3"])
-        Dihedral = namedtuple("Length", ["type", "atom1", "atom2", "atom3", "atom4"])
-        Improper = namedtuple("Length", ["type", "atom1", "atom2", "atom3", "atom4"])
 
         while True:
             mol = fp.nextsection()
@@ -37,25 +33,15 @@ class MolDatabase:
                         toks[2] = "0"
                     self.molecules[mol].atoms[toks[0]] = Atom.frommoldb(toks[0], toks[1], float(toks[2]))
 
-            if nbnds is not None:
-                for i in range(int(nbnds)):
-                    toks = fp.getline()
-                    self.molecules[mol].lengths.append(Length(toks[0], toks[1], toks[2]))
+            self.get_bonds(fp, nbnds, self.molecules[mol].lengths)
+            self.get_bonds(fp, nangs, self.molecules[mol].angles)
+            self.get_bonds(fp, ndihs, self.molecules[mol].dihedrals)
+            self.get_bonds(fp, nimps, self.molecules[mol].impropers)
 
-            if nangs is not None:
-                for i in range(int(nangs)):
-                    toks = fp.getline()
-                    self.molecules[mol].angles.append(
-                            Angle(toks[0], toks[1], toks[2], toks[3]))
-
-            if ndihs is not None:
-                for i in range(int(ndihs)):
-                    toks = fp.getline()
-                    self.molecules[mol].dihedrals.append(
-                            Dihedral(toks[0], toks[1], toks[2], toks[3], toks[4]))
-
-            if nimps is not None:
-                for i in range(int(nimps)):
-                    toks = fp.getline()
-                    self.molecules[mol].impropers.append(
-                            Improper(toks[0], toks[1], toks[2], toks[3], toks[4]))
+    @staticmethod
+    def get_bonds(fp, n, store):
+        Bond = namedtuple("Bond", ["type", "atom1", "atom2", "atom3", "atom4"])
+        if n is not None:
+            for i in range(int(n)):
+                toks = fp.getline(5)
+                store.append(Bond(*toks))
