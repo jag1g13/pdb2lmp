195455f8bd450173479df4cf47a22abafff5d26a
James Graham <J.A.Graham@soton.ac.uk>
2016-01-20 17:02:46 +0000

    Refactored BondDatabase - less code repetition

--- lib/bonddatabase.py
+++ lib/bonddatabase.py
@@ -12,30 +12,17 @@ class BondDatabase:
         self.dihedrals = {}
         self.impropers = {}
 
+        self.section(fp, "length", self.lengths)
+        self.section(fp, "angle", self.angles)
+        self.section(fp, "dihedral", self.dihedrals)
+        self.section(fp, "improper", self.impropers)
+
+    @staticmethod
+    def section(fp, name, adddict):
         Param = namedtuple("Param", ["style", "params"])
-
-        # Read bond lengths
         while True:
-            toks = fp.getlinefromsection("length")
+            toks = fp.getlinefromsection(name)
             if toks is None:
                 break
-            self.lengths[toks[0]] = Param(toks[1], " ".join(toks[2:]))
+            adddict[toks[0]] = Param(toks[1], " ".join(toks[2:]))
 
-        # Read angles
-        while True:
-            toks = fp.getlinefromsection("angle")
-            if toks is None:
-                break
-            self.angles[toks[0]] = Param(toks[1], " ".join(toks[2:]))
-
-        while True:
-            toks = fp.getlinefromsection("dihedral")
-            if toks is None:
-                break
-            self.dihedrals[toks[0]] = Param(toks[1], " ".join(toks[2:]))
-
-        while True:
-            toks = fp.getlinefromsection("improper")
-            if toks is None:
-                break
-            self.impropers[toks[0]] = Param(toks[1], " ".join(toks[2:]))
