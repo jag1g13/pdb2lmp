7430508c15e15487f78c80b6b2c4f16a32c8dfbc
James Graham <J.A.Graham@soton.ac.uk>
2016-06-01 16:36:16 +0100

    Changed all file.write to print(file=) and cleaned

--- pdb2lmp.py
+++ pdb2lmp.py
@@ -10,7 +10,9 @@ from lib.bonddatabase import BondDatabase
 
 
 class NonMatchingAtomException(Exception):
-    pass
+    def __init__(self, num, name1, name2):
+        line = "Atom {0} in coordinate file ({1}) does not match atom in force field ({2})."
+        super(NonMatchingAtomException, self).__init__(line.format(num, name1, name2))
 
 
 class PolymerError(Exception):
@@ -102,8 +104,7 @@ class PDB2LMP:
                     if self._suppress_atom_names:
                             coordfile_atom.name = dbmol_atom.name
                     else:
-                        raise NonMatchingAtomException("Atom {0} in coordinate file ({1}) does not match atom in force field ({2}).".
-                                                       format(atnum, coordfile_atom.name, dbmol_atom.name))
+                        raise NonMatchingAtomException(atnum, coordfile_atom.name, dbmol_atom.name)
                 self.natoms.total += 1
                 atnum += 1
 
@@ -116,45 +117,46 @@ class PDB2LMP:
 
     def write_data(self, filename):
         with open(filename, "w") as data:
-            data.write("LAMMPS 'data.' input file created by PDB2LMP\n")
-            data.write("\n")
-            data.write("{0:8d} atoms\n".format(self.natoms.total))
-            data.write("{0:8d} bonds\n".format(self.nlengths.total))
-            data.write("{0:8d} angles\n".format(self.nangles.total))
-            data.write("{0:8d} dihedrals\n".format(self.ndihedrals.total))
-            data.write("{0:8d} impropers\n".format(self.nimpropers.total))
-            data.write("\n")
-            data.write("{0:8d} atom types\n".format(self.natoms.types))
-            data.write("{0:8d} bond types\n".format(self.nlengths.types))
-            data.write("{0:8d} angle types\n".format(self.nangles.types))
-            data.write("{0:8d} dihedral types\n".format(self.ndihedrals.types))
-            data.write("{0:8d} improper types\n".format(self.nimpropers.types))
-            data.write("\n")
+            print("LAMMPS 'data.' input file created by PDB2LMP", file=data)
+            print(file=data)
+            print("{0:8d} atoms".format(self.natoms.total), file=data)
+            print("{0:8d} bonds".format(self.nlengths.total), file=data)
+            print("{0:8d} angles".format(self.nangles.total), file=data)
+            print("{0:8d} dihedrals".format(self.ndihedrals.total), file=data)
+            print("{0:8d} impropers".format(self.nimpropers.total), file=data)
+            print(file=data)
+            print("{0:8d} atom types".format(self.natoms.types), file=data)
+            print("{0:8d} bond types".format(self.nlengths.types), file=data)
+            print("{0:8d} angle types".format(self.nangles.types), file=data)
+            print("{0:8d} dihedral types".format(self.ndihedrals.types), file=data)
+            print("{0:8d} improper types".format(self.nimpropers.types), file=data)
+            print(file=data)
             cell = [val / 2 for val in self.coords.cell]
-            data.write("{0:8.3f} {1:8.3f} xlo xhi\n".format(-cell[0], cell[0]))
-            data.write("{0:8.3f} {1:8.3f} ylo yhi\n".format(-cell[1], cell[1]))
-            data.write("{0:8.3f} {1:8.3f} zlo zhi\n".format(-cell[2], cell[2]))
-            data.write("\n")
-            data.write("Atoms\n")
-            data.write("\n")
+            print("{0:8.3f} {1:8.3f} xlo xhi".format(-cell[0], cell[0]), file=data)
+            print("{0:8.3f} {1:8.3f} ylo yhi".format(-cell[1], cell[1]), file=data)
+            print("{0:8.3f} {1:8.3f} zlo zhi".format(-cell[2], cell[2]), file=data)
+            print(file=data)
+            print("Atoms", file=data)
+            print(file=data)
+
+            atomline = "{0:6d} {1:4d} {2:8.3f} {3:8.3f} {4:8.3f} {5:4d} {6:5.2f} {7:8.3f} {8:8.3f} {9:8.3f} {10:5.2f} {11:5.2f}"
             for i, atom in enumerate(self.coords.atoms, start=1):
                 # Write atom line
                 # Dipoles are all oriented up - this should equilibrate out quickly
-                data.write("{0:6d} {1:4d} {2:8.3f} {3:8.3f} {4:8.3f} {5:4d} {6:5.2f} {7:8.3f} {8:8.3f} {9:8.3f} {10:5.2f} {11:5.2f}\n".format(
-                    i, self.atomtypes.index(atom.type)+1, atom.x, atom.y, atom.z,
-                    atom.resid, atom.charge, atom.dipole, 0, 0, atom.diameter, atom.rotmass
-                ))
+                print(atomline.format(i, self.atomtypes.index(atom.type)+1,
+                                      atom.x, atom.y, atom.z, atom.resid, atom.charge,
+                                      atom.dipole, 0, 0, atom.diameter, atom.rotmass), file=data)
 
             def write_bonds(n, types, header):
                 if n <= 0:
                     return
-                data.write("\n" + header + "\n\n")
+                print("\n" + header + "\n", file=data)
                 i = 1
                 for ii, mol in enumerate(self.coords.molecules):
                     mol_db = self.moldb.molecules[mol.name]
                     atom_list = list(mol_db.atoms.keys())
                     for bond in getattr(mol_db, header.lower()):
-                        data.write("{0:6d} {1:4d}".format(i, types.index(bond.type) + 1))
+                        print("{0:6d} {1:4d}".format(i, types.index(bond.type) + 1), file=data, end="")
                         for atom in bond.atoms:
                             try:
                                 atom_num = mol.atoms[atom_list.index(atom)]
@@ -176,8 +178,8 @@ class PDB2LMP:
                                 other_atom_list = list(other_mol_db.atoms.keys())
                                 atom_num = other_mol.atoms[other_atom_list.index(atom[1:])]
 
-                            data.write(" {0:6d}".format(atom_num + 1))
-                        data.write("\n")
+                            print(" {0:6d}".format(atom_num + 1), file=data, end="")
+                        print(file=data)
                         i += 1
 
             write_bonds(self.nlengths.total, self.lentypes, "Bonds")
@@ -187,52 +189,51 @@ class PDB2LMP:
 
     def write_forcefield(self, filename):
         with open(filename, "w") as ff:
-            ff.write("# Forcefield prepared by PDB2LMP\n")
-            ff.write("\n")
+            print("# Forcefield prepared by PDB2LMP", file=ff)
+            print(file=ff)
             # TODO change these to "0.0 1.0 1.0 12.0" - ELBA standard
-            ff.write("pair_style lj/sf/dipole/sf 12.0\n")
-            ff.write("special_bonds lj/coul 0.0 0.0 0.0\n")
+            print("pair_style lj/sf/dipole/sf 12.0", file=ff)
+            print("special_bonds lj/coul 0.0 0.0 0.0", file=ff)
+            print(file=ff)
 
             def write_styles(styles, header):
                 if styles:
-                    ff.write(header)
+                    print(header, file=ff, end="")
                     for style in styles:
-                        ff.write(" " + style)
-                    ff.write("\n")
+                        print(" " + style, file=ff, end="")
+                    print(file=ff)
 
             write_styles(self.lenstyles, "bond_style hybrid")
             write_styles(self.angstyles, "angle_style hybrid")
             write_styles(self.dihstyles, "dihedral_style hybrid")
             write_styles(self.impstyles, "improper_style hybrid")
 
-            ff.write("\n")
+            print(file=ff)
+            line = "mass {0:4d} {1:8.3f} # {2}"
             for i, atomtype in enumerate(self.atomtypes, start=1):
-                ff.write("mass {0:4d} {1:8.3f} # {2}\n".format(
-                    i, self.atomdb.atoms[atomtype].mass, atomtype
-                ))
+                print(line.format(i, self.atomdb.atoms[atomtype].mass, atomtype), file=ff)
 
-            ff.write("\n")
+            print(file=ff)
+            line = "set type{0:4d} diameter {1:8.3f} # {2}"
             for i, atomtype in enumerate(self.atomtypes, start=1):
-                ff.write("set type {0:4d} diameter {1:8.3f} # {2}\n".format(
-                        i, self.atomdb.atoms[atomtype].diameter, atomtype
-                ))
+                print(line.format(i, self.atomdb.atoms[atomtype].diameter, atomtype), file=ff)
 
-            ff.write("\n")
+            print(file=ff)
+            line = "pair_coeff {0:4d} {1:4d} {2:6.3f} {3:6.3f} # {4}-{5}"
             for i, atomtype in enumerate(self.atomtypes, start=1):
                 for j, atomtype2 in enumerate(self.atomtypes, start=1):
                     if i > j:
                         continue
                     sig, eps = self.atomdb.lj(atomtype, atomtype2)
-                    ff.write("pair_coeff {0:4d} {1:4d} {2:6.3f} {3:6.3f} # {4}-{5}\n".format(
-                        i, j, eps, sig, atomtype, atomtype2
-                    ))
+                    print(line.format(i, j, eps, sig, atomtype, atomtype2), file=ff)
 
             def write_types(types, db_vals, line_prefix):
-                if types is not []:
-                    ff.write("\n")
+                if types:
+                    print(file=ff)
+                    line = line_prefix + " {0:4d} {1} {2} # {3}"
                     for i, tipe in enumerate(types, start=1):
-                        ff.write(line_prefix + " {0:4d} {1} {2} # {3}\n".format(
-                            i, db_vals[tipe].style, db_vals[tipe].params, tipe))
+                        db_type = db_vals[tipe]
+                        print(line.format(i, db_type.style, db_type.params, tipe), file=ff)
 
             write_types(self.lentypes, self.bonddb.length, "bond_coeff")
             write_types(self.angtypes, self.bonddb.angle, "angle_coeff")
